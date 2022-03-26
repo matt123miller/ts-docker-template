6938d26fb6c284284cea1377ee7252b3ccdd026d
Matt Miller <matt123miller@hotmail.co.uk>
2022-03-26 15:19:48 +0000

    supporting env specific tsconfig builds

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     sourceType: "module",
-    project: "./tsconfig.json",
+    project: "./tsconfig.dev.json",
   },
   ignorePatterns: [
     "src/**/*.test.ts", 
