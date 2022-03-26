948b045c65dd5905f91a5ffa874395fae3704a90
Matt Miller <matt123miller@hotmail.co.uk>
2022-03-26 15:14:20 +0000

    Set up TS, linting, recompile on save, build.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  ignorePatterns: [
+    "src/**/*.test.ts", 
+    "build/*.js", 
+    ".*.js", 
+    ".*.ts"
+  ],
+  extends: [
+    // My set of defaults
+    "matt123miller",
+    // Add the default recommendations @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended", 
+    // Add prettier settings to avoid rules conflicts @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", 
+    // Add prettier plugin
+    "plugin:prettier/recommended", 
+  ],
+};
